dac88f45c01a66f6e81e95ede8bd329d7dc40129
Tyler Anderson <andersontyler65@gmail.com>
2017-09-26 13:34:51 -0600

    devstart added indentation is aesome

--- routes/movie.js
+++ routes/movie.js
@@ -1,21 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 const knex = require('../knex')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   knex('movie')
-    .select('id', 'title', 'year', 'director_id')
-    .orderBy('id')
-    .then((items) => {
-       res.setHeader ('Content-Type', 'application/json')
-      res.send(JSON.stringify(items));
-    })
-    .catch((err) => next(err))
-    console.log('in movie.js')
+  .select('id', 'title', 'year', 'director_id')
+  .orderBy('id')
+  .then((items) => {
+    res.setHeader ('Content-Type', 'application/json')
+    res.send(JSON.stringify(items))
+  })
+  .catch((err) => next(err))
 });
-// if (items.length<1) {
-//   return res.sendStatus(404)
-// }
 
-module.exports = router;
+module.exports = router
